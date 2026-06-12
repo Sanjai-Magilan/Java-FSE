1c162e7fd6f8ce186491112feae819575f571113
Sanjai-Magilan <sanjai6115@gmail.com>
2026-06-12 14:12:06 +0530

    properly tested Singleton

--- java/Module1/SingletonPatternExample/Logger.java
+++ java/Module1/SingletonPatternExample/Logger.java
@@ -19,12 +19,3 @@ class Logger{
 }
 
 
-public class Singleton {
-    public static void main(String[] args) {
-        Logger L1 = Logger.getInstance();
-        L1.PrintLog("Application is running");
-        Logger L2 = Logger.getInstance();
-        L2.PrintLog("Application crashed !");
-        System.out.println("L1 = L2 : "+(L1==L2));
-    }
-}
