aac216afe2ba85d987d881668d4b54f249179c91
Sanjai-Magilan <sanjai6115@gmail.com>
2026-06-12 11:55:26 +0530

    Singleton Pattern Done

---
+++ src/main/java/SingletonPatternExample/single.java
@@ -0,0 +1,30 @@
+package SingletonPatternExample;
+
+class Logger{
+
+    private  static Logger instance;
+    private Logger(){
+        System.out.println("Instance created !");
+    }
+
+    public static Logger getInstance(){
+        if(instance == null)
+            instance = new Logger();
+        return instance;
+    }
+
+    public static void PrintLog(String log){
+        System.out.println("Log : "+log);
+    }
+}
+
+
+public class single {
+    public static void main(String[] args) {
+        Logger L1 = Logger.getInstance();
+        L1.PrintLog("Application is running");
+        Logger L2 = Logger.getInstance();
+        L2.PrintLog("Application crashed !");
+        System.out.println("L1 = L2 : "+(L1==L2));
+    }
+}
